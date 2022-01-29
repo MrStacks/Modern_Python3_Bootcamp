54dbb06bf2d9836b567910590f402e8a1624fce3
MrStacks <keithstack@hotmail.com>
2022-01-29 11:38:22 -0600

    completed how to parameterize inputs

--- dynamic_queries.py
+++ dynamic_queries.py
@@ -1,15 +1,27 @@
 '''Video 377 - SQL Injection!
 '''
 import sqlite3
-
 connection = sqlite3.connect("users.db")
-# create cursor object
+u = input("Please enter your username... ")
+p = input("Please enter your password... ")
+
 c = connection.cursor()
 
+# # The WRONG way: 
+# query = f"SELECT * FROM users WHERE username='{u}' AND password='{p}'" 
+# '''The [problem] is, because we're not parameterizing our inputs, we're 
+# just sticking them directly into the SQL query, a user could actually 
+# write SQL as part of their password.'''
 
-# commit changes
+ # Therefore, the RIGHT way (SQL library prevents malicious injections): 
+query = f"SELECT * FROM users WHERE username=? AND password=?"
+c.execute(query, (u,p))
+
+result = c.fetchone()
+if (result):
+    print("WELCOME BACK")
+else:
+    print("FAILED LOGIN")    
+
 connection.commit()
-
-# the above commands look the same as what we have seen
-# in this case we'll get something back, however
 connection.close()
